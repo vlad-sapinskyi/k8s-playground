048a1b043f0b89eb718ee3d73ec596db87a89b86
Vlad Sapinskyi <vlds@simcorp.com>
2026-07-24 11:35:11 +0200

    fix: check if distro is installed

--- clusters/wsl/Test-Wsl.ps1
+++ clusters/wsl/Test-Wsl.ps1
@@ -16,9 +16,16 @@ Write-Verbose "Checking WSL installation status..."
 wsl --status 2>&1 | Out-Null
 $wslInstalled = ($LASTEXITCODE -eq 0)
 
+Write-Verbose "Checking if distro '$Distro' is registered..."
+$distroInstalled = $false
+if ($wslInstalled) {
+    $registeredDistros = (wsl -l -q) 2>&1
+    $distroInstalled = ($registeredDistros -contains $Distro)
+}
+
 switch ($Action) {
     'Install' {
-        if (-not $wslInstalled) {
+        if ((-not $wslInstalled) -or (-not $registeredDistros)) {
             Write-Host "Installing..."
             wsl --install -d $Distro
             exit 0
@@ -34,7 +41,9 @@ switch ($Action) {
     'Uninstall' {
         if ($wslInstalled) {
             Write-Host "Uninstalling..."
-            wsl --unregister $Distro 
+            if ($distroInstalled) {
+                wsl --unregister $Distro 
+            }
             wsl --uninstall
             exit 0
         }
@@ -44,8 +53,12 @@ switch ($Action) {
 if (-not $wslInstalled) {
     Write-Host "WSL is not installed:"
     Write-Host "    Re-run this script with '-Action Install' parameter."
+} elseif (-not $distroInstalled) {
+    Write-Host "WSL is installed, but '$Distro' is not registered:"
+    Write-Host "    Re-run this script with '-Action Install' parameter."
 } else {
     Write-Host "WSL is already installed:"
     $versionInfo = wsl --version
     Write-Host "    $($versionInfo[0])"
+    Write-Host "    distro '$Distro' is registered"
 }
