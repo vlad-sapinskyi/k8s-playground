9002625d89b00079808938d56bfd1760d4db00b7
Vlad Sapinskyi <vlds@simcorp.com>
2026-08-17 14:43:06 +0200

    feat: application di

---
+++ app/src/Todo.Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace Todo.Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(cfg =>
+            cfg.AddMaps(Assembly.GetExecutingAssembly()));
+
+        builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        builder.Services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+    }
+}
